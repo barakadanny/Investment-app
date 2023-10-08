1e7901ba6fb3838d30513d116e5391c9696b4d3b
Danny <barakadan421@gmail.com>
2023-10-09 00:52:26 +0300

    Handle form submit `interest calculation`

--- src/components/NewInvestment/InvestmentForm.js
+++ src/components/NewInvestment/InvestmentForm.js
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InvestmentForm() {
+  const [enteredCurrentSavings, setEnteredCurrentSavings] = useState("");
+  const [enteredYearlySavings, setEnteredYearlySavings] = useState("");
+  const [enteredExpectedInterest, setEnteredExpectedInterest] = useState("");
+  const [enteredDuration, setEnteredDuration] = useState("");
+
+  const currentSavingsChangeHandler = (event) => {
+    setEnteredCurrentSavings(event.target.value);
+  };
+
+  const yearlySavingsChangeHandler = (event) => {
+    setEnteredYearlySavings(event.target.value);
+  };
+
+  const expectedInterestChangeHandler = (event) => {
+    setEnteredExpectedInterest(event.target.value);
+  };
+
+  const durationChangeHandler = (event) => {
+    setEnteredDuration(event.target.value);
+  };
+
+  const CalculateInterestHandler = (event) => {
+    event.preventDefault();
+    const investmentData = {
+      currentSavings: enteredCurrentSavings,
+      yearlySavings: enteredYearlySavings,
+      expectedInterest: enteredExpectedInterest,
+      duration: enteredDuration,
+    };
+
+    console.log(investmentData);
+  };
   return (
     <div>
       <form className="form">
         <div className="input-group">
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
+            <input
+              type="number"
+              id="current-savings"
+              value={enteredCurrentSavings}
+              onChange={currentSavingsChangeHandler}
+            />
           </p>
           <p>
             <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
+            <input
+              type="number"
+              value={enteredYearlySavings}
+              onChange={yearlySavingsChangeHandler}
+              id="yearly-contribution"
+            />
           </p>
         </div>
         <div className="input-group">
@@ -19,18 +61,32 @@ function InvestmentForm() {
             <label htmlFor="expected-return">
               Expected Interest (%, per year)
             </label>
-            <input type="number" id="expected-return" />
+            <input
+              value={enteredExpectedInterest}
+              onChange={expectedInterestChangeHandler}
+              type="number"
+              id="expected-return"
+            />
           </p>
           <p>
             <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
+            <input
+              value={enteredDuration}
+              onChange={durationChangeHandler}
+              type="number"
+              id="duration"
+            />
           </p>
         </div>
         <p className="actions">
           <button type="reset" className="buttonAlt">
             Reset
           </button>
-          <button type="submit" className="button">
+          <button
+            type="submit"
+            onClick={CalculateInterestHandler}
+            className="button"
+          >
             Calculate
           </button>
         </p>
