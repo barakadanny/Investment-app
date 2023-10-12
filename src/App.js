1e73b8d2aa4d614a8f44c572053b2cc0eec7b000
Danny <barakadan421@gmail.com>
2023-10-12 18:11:16 +0300

    Update app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
 import logo from "./assets/investment-calculator-logo.png";
 import InvestmentCalculator from "./components/Investments/InvestmentCalculator";
 
+const DUMMY_INVESTMENTS = [
+  {
+    id: "e1",
+    currentSavings: 654,
+    yearlySavings: 100,
+    expectedInterest: 5,
+    duration: 3,
+  },
+];
+
 function App() {
+  const [investments, setInvestments] = useState(DUMMY_INVESTMENTS);
+
+  const addInvestmentHandler = (investment) => {
+    setInvestments((prevInvestments) => {
+      return [investment, ...prevInvestments];
+    });
+    // console.log(investment);
+  };
   return (
     <div>
-      <InvestmentCalculator />
+      <InvestmentCalculator
+        items={investments}
+        addInvestment={addInvestmentHandler}
+      />
     </div>
   );
 }
